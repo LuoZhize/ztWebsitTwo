843f77188404d9810e98c6f8904c484b7dad73b2
LuoZhize <1518879327@qq.com>
2018-08-08 18:07:09 +0800

    header code

---
+++ js/index.js
@@ -0,0 +1,13 @@
+//导入页面
+$('#ztFWPro').load("./fwPro.html",function () {
+    jQuery.getScript("../ztWebsiteTwo/js/ztFWPro.js");
+});
+$('#ztProAL').load("./proAl.html",function () {
+    jQuery.getScript("../ztWebsiteTwo/js/ztProAL.js");
+});
+$('#ztNewGG').load("./newGg.html",function () {
+    jQuery.getScript("../ztWebsiteTwo/js/ztNewGG.js");
+});
+$('#ztWmWay').load("./wmWay.html",function () {
+    jQuery.getScript("../ztWebsiteTwo/js/ztWmWay.js");
+});
