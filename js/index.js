44b0383bb13e5e4b3b05e61207bb28ee081ea75e
LuoZhize <1518879327@qq.com>
2018-08-13 18:03:39 +0800

    add compatible

--- js/index.js
+++ js/index.js
@@ -1,28 +1,34 @@
-var winHeight = $(window).height();//获取浏览器可视区域的高度
-setPageHeight(winHeight);
-$(window).scroll(function () {
-    var after = $(window).scrollTop();//滚动之后的位置
-    if (after > 650) {//鼠标滚动到指定位置时导航固定改变颜色
-        $('.ztNav').css({'position': 'fixed', 'background': '#16233D'});
-    } else if (after < 600) {
-        $('.ztNav').css({'position': 'relative', 'background': 'transparent'});
-    }
-})
-$('.ztNavList li').eq(0).click(function () {//点击切换时导航栏的颜色
-    $('.ztNav').css('background', 'transparent');
-}).siblings().click(function () {
-    $('.ztNav').css('background', '#16233D');
-})
+$(document).ready(function () {
+    $('.container').show();//在dom树加载完成后在加载
+    var winHeight = $(window).height();//获取浏览器可视区域的高度
+    setPageHeight(winHeight);
+    $(window).scroll(function () {
+        var after = $(window).scrollTop();//滚动之后的位置
+        if (after > 650) {//鼠标滚动到指定位置时导航固定改变颜色
+            $('.ztNav').css({'position': 'fixed', 'background': '#16233D'});
+        } else if (after < 600) {
+            $('.ztNav').css({'position': 'relative', 'background': 'transparent'});
+        }
+        if (after > 1000) {//返回顶部
+            $('.up-top').css('display','block');
+        } else {
+            $('.up-top').css('display','none');
+        }
+    })
+    $('.ztNavList li').eq(0).click(function () {//点击切换时导航栏的颜色
+        $('.ztNav').css('background', 'transparent');
+    }).siblings().click(function () {
+        $('.ztNav').css('background', '#16233D');
+    })
 
-function setPageHeight(a) {//设置每个分页的最小高度
-    $('.ztHp').css('min-height', a + "px");
-    $("#map").css('min-width', a + "px");
-}
+    function setPageHeight(a) {//设置每个分页的最小高度
+        $('.ztHp').css('min-height', a + "px");
+        $("#map").css('min-width', a + "px");
+    }
 
 //初始化wow
-new WOW().init();
+    new WOW().init();
 //项目案例
-$(document).ready(function () {
     var $container = $('.isotope').isotope({//初始化
         transitionDuration: '0.6s',
         itemSelector: '.element-item'
@@ -52,24 +58,29 @@ $(document).ready(function () {
         $item.addClass('item-info');
         return $item;
     }
-});
+
 //联系我们
-var map = new BMap.Map("map");//百度地图
-var point = new BMap.Point(116.543865, 39.91278);//中心点经纬
-map.centerAndZoom(point, 15);//初始化地图①设置地图的中心点，②缩放范围3-19
-map.enableScrollWheelZoom(true); // 开启鼠标滚轮缩放
-addMarker(point);
+    var map = new BMap.Map("map");//百度地图
+    var point = new BMap.Point(116.543865, 39.91278);//中心点经纬
+    map.centerAndZoom(point, 15);//初始化地图①设置地图的中心点，②缩放范围3-19
+    map.enableScrollWheelZoom(true); // 开启鼠标滚轮缩放
+    addMarker(point);
 
-function addMarker(point) {
-    var iconSize = new BMap.Size(30, 35);//图标可视区域的大小
-    var iconPath = "../ztWebsitTwo/img/mapBiaoZhu.png";//图标路径
-    var myIcon = new BMap.Icon(iconPath, iconSize, {
-        anchor: new BMap.Size(10, 35),// 指定定位位置
-    })
-    var marker = new BMap.Marker(point, {icon: myIcon});//创建标注对象
-    map.addOverlay(marker);//添加到地图
-}
+    function addMarker(point) {
+        var iconSize = new BMap.Size(30, 35);//图标可视区域的大小
+        var iconPath = "../ztWebsitTwo/img/mapBiaoZhu.png";//图标路径
+        var myIcon = new BMap.Icon(iconPath, iconSize, {
+            anchor: new BMap.Size(10, 35),// 指定定位位置
+        })
+        var marker = new BMap.Marker(point, {icon: myIcon});//创建标注对象
+        map.addOverlay(marker);//添加到地图
+    }
 
-$('#map-p>img').click(function () {
-    map.reset();//恢复地图
+    $('#map-p>img').click(function () {
+        map.reset();//恢复地图
+    });
+});
+//点击返回顶部
+$('.up-top').click(function () {
+    $('body,html').animate({scrollTop:0},1000);
 });
