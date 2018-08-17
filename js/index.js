f5d462a3a697fa374ccf5c3d41fcfde74e1fa040
LuoZhize <1518879327@qq.com>
2018-08-17 13:26:08 +0800

    UFOChange

--- js/index.js
+++ js/index.js
@@ -3,10 +3,11 @@ setTimeout(function () {
         $('.container').show();//在dom树加载完成后在加载
         $('.ztHp').show();
         $('.zt-footer').show();
-        var winHeight = $(window).height();//获取浏览器可视区域的高度
+        let winHeight = $(window).height(),//获取浏览器可视区域的高度、宽度
+            sevWidth = $(window).width();
         setPageHeight(winHeight);
         $(window).scroll(function () {
-            var after = $(window).scrollTop();//滚动之后的位置
+            let after = $(window).scrollTop();//滚动之后的位置
             if (after > 650) {//鼠标滚动到指定位置时导航固定改变颜色
                 $('.ztNav').css({'position': 'fixed', 'background': '#16233D'});
             } else if (after < 600) {
@@ -32,13 +33,13 @@ setTimeout(function () {
 
         new WOW().init();//初始化wow
         //项目案例
-        var $container = $('.isotope').isotope({//初始化
+        let $container = $('.isotope').isotope({//初始化
             transitionDuration: '0.6s',
             itemSelector: '.element-item'
         });
         $container.imagesLoaded(function () {//预加载
             $('#filters').on('click', 'button', function () {
-                var filterValue = $(this).attr('data-filter');
+                let filterValue = $(this).attr('data-filter');
                 $container.isotope({filter: filterValue});
             });
         });
@@ -51,31 +52,31 @@ setTimeout(function () {
             }, 100)
             setTimeout(function () {//动画
                 $('.zt-new-loader').hide();
-                var $item = getItemEle().add(getItemEle()).add(getItemEle());//添加图片
+                let $item = getItemEle().add(getItemEle()).add(getItemEle());//添加图片
                 $container.append($item).isotope('appended', $item);
             }, 1000)
         })
 
         function getItemEle() {//需要返回 获取样式和js
-            var $item = $('<div class="element-item"><a><img src="../ztWebsitTwo/img/pro1.png" alt=""></a></div>');
+            let $item = $('<div class="element-item"><a><img src="../ztWebsitTwo/img/pro1.png" alt=""></a></div>');
             $item.addClass('item-info');
             return $item;
         }
 
         //联系我们
-        var map = new BMap.Map("map");//百度地图
-        var point = new BMap.Point(116.543865, 39.91278);//中心点经纬
+        let map = new BMap.Map("map");//百度地图
+        let point = new BMap.Point(116.543865, 39.91278);//中心点经纬
         map.centerAndZoom(point, 15);//初始化地图①设置地图的中心点，②缩放范围3-19
         map.enableScrollWheelZoom(true); // 开启鼠标滚轮缩放
         addMarker(point);
 
         function addMarker(point) {
-            var iconSize = new BMap.Size(30, 35);//图标可视区域的大小
-            var iconPath = "../ztWebsitTwo/img/mapBiaoZhu.png";//图标路径
-            var myIcon = new BMap.Icon(iconPath, iconSize, {
+            let iconSize = new BMap.Size(30, 35),//图标可视区域的大小
+                iconPath = "../ztWebsitTwo/img/mapBiaoZhu.png";//图标路径
+            let myIcon = new BMap.Icon(iconPath, iconSize, {
                 anchor: new BMap.Size(10, 35),// 指定定位位置
             })
-            var marker = new BMap.Marker(point, {icon: myIcon});//创建标注对象
+            let marker = new BMap.Marker(point, {icon: myIcon});//创建标注对象
             map.addOverlay(marker);//添加到地图
         }
 
@@ -87,12 +88,14 @@ setTimeout(function () {
             $('body,html').animate({scrollTop: 0}, 500);
         });
         //首页动画
-        var sevWidth = $(window).width();
-        var sevHeight = $(window).height();
-        let p = 100, deg = 1.2, a = 1;
-        let myTimer = null;
-        let x = sevWidth / 1.5, y;
-        let incX = -1.00000002;
+        let animateNum = 0,
+            p = 100,//焦点
+            deg = 1.2,//角度
+            myTimer = null,//时间
+            x = sevWidth / 1.5,//left
+            y,//top
+            incX = -1;//方向
+        let moveD = document.getElementById('moveD');
 
         function moveObj() {//清除定时器
             if (myTimer != null) {
@@ -101,10 +104,7 @@ setTimeout(function () {
             myTimer = setInterval(goStep, 1);
         }
 
-        let moveD = document.getElementById('moveD');
-
         function goStep() {//moveLine
-            a += 0.05;
             x = x + incX;
             if (x < 0) {
                 incX = 1;
@@ -117,17 +117,31 @@ setTimeout(function () {
             y = -Math.sqrt(deg * p * x);
             if (incX == 1) {
                 y = -1 * y;
-                a += 0.4;
             }
-            y += sevHeight / 1.7;
-            if (y >= sevHeight / 1.28) {
-                $('#moveD').animate({left: sevWidth / 2.6 + "px"}, 1500).fadeOut(1000);
-                $('.ztHp_animate').fadeIn(500).addClass('move-line-animate');
-                return y = sevHeight / 1.28;
+            y += winHeight / 1.7;
+            if (y >= winHeight / 1.28) {
+                $('#moveD').animate({left: sevWidth / 2.6 + "px"}, 1500).fadeOut(1000);//横走
+                $('.ztHp_animate').fadeIn(500).addClass('move-line-animate');//添加class动画
+                return y = winHeight / 1.28;//固定y高度
             }
-            moveD.style.top = y + "px";
-            moveD.style.left = x + "px";
-            moveD.style.transform = "rotateY(" + a + "deg)";
+            if (x <= sevWidth / 20) {//转弯动画
+                animateNum++;
+                if (animateNum <= 1) {
+                    for (let times2 = 0; times2 < 32; times2++) {
+                        let a2 = sevWidth / 20.002, b2 = winHeight / 1.605, r2 = 135;//第二个a,b圆心;r半径
+                        let huDu2 = 15 * (Math.PI / 360) * times2;//度数
+                        let X2 = a2 - Math.sin(huDu2) * r2;
+                        let Y2 = b2 - Math.cos(huDu2) * r2;
+                        $('#moveD').animate({left: X2 + 'px', top: Y2 + 'px'}, 50, 'linear', function () {
+
+                        });
+                    }
+                } else {
+                }
+            } else {
+                moveD.style.top = y + "px";
+                moveD.style.left = x + "px";
+            }
             //描绘
             // let divObj = document.createElement("div");
             // let cssStr = "position:absolute;width:10px;height:10px;background-color:red;";
@@ -138,4 +152,4 @@ setTimeout(function () {
 
         moveObj();
     });
-}, 500)
+}, 800)
